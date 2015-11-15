32cf4fc9a10aa03a1a58726e826b9dd7630531d8
Timothy Moran <tyrsius@gmail.com>
2015-11-15 01:47:08 -0800

    fix server issue:

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-gvar port = process.env.PORT || 9000,
+var port = process.env.PORT || 9000,
     clientDir = __dirname + '/src/',
     assetDir = __dirname + '/assets',
     jspmConfigName = '/jspm.config.js',
