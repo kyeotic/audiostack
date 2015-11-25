9fb0882b321fe3fb5878b3ec53ad85d51f072510
tyrsius <tyrsius@gmail.com>
2015-11-25 09:19:17 -0800

    make AudioSource replayable

--- src/audio/audio-source.js
+++ src/audio/audio-source.js
@@ -1,11 +1,34 @@
 import audioContext from './audio-context';
 import {bufferToAudioBuffer} from './audio-buffer-encoder';
 
-export function sourceFromAudioBuffer(audioBuffer) {
+class ReusableAudioSourceNode {
+	constructor (audioBuffer) {
+		this.buffer = audioBuffer;
+	}
+
+	start(startTime = 0) {
+		//Do not allow multiple sources to play at the same time
+		if (this.source) return;
+		this.source = _sourceFromAudioBuffer(this.buffer);
+		this.source.start(startTime);
+	}
+
+	stop() {
+		this.source.stop();
+		this.source = null
+	}
+}
+
+
+function _sourceFromAudioBuffer(audioBuffer) {
 	var source = audioContext.createBufferSource();
 	source.buffer = audioBuffer;
 	source.connect(audioContext.destination);
 	return source;
+}
+
+export function sourceFromAudioBuffer(audioBuffer) {
+	return new ReusableAudioSourceNode(audioBuffer);
 }
 
 export function sourceFromBuffer(buffer) {
