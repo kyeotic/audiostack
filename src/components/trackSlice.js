605ff47f99a67fe9e76feaaf165d32edd533f66c
tyrsius <tyrsius@gmail.com>
2016-01-24 16:40:39 -1000

    add fine slice control

--- src/components/trackSlice.js
+++ src/components/trackSlice.js
@@ -17,7 +17,7 @@ export default class TrackSlice extends Component {
 		super(...args);
 		this.minValue = 0;
 		this.maxValue = Math.floor(this.props.source.buffer.duration);
-		this.state = {values: [this.minValue, this.maxValue]};
+		this.state = {values: [this.minValue, this.maxValue], subSlice: false};
 	}
 
 	createSlice = () => {
@@ -48,19 +48,31 @@ export default class TrackSlice extends Component {
 		this.setState({values: values});
 	}
 
+	toggleSubSlice = () => {
+		this.setState({
+			subSlice : !this.state.subSlice,
+			subSliceValues: this.state.values.slice(0)
+		});
+	}
+
 	render() {
+		let maxValue = this.state.subSlice ? this.state.subSliceValues[1] : this.maxValue,
+			minValue = this.state.subSlice ? this.state.subSliceValues[0] : this.minValue;
+
 		return (			
 			<div>
 				<button type="button" className="btn" onClick={this.preview}>{'Preview'}</button>
 				<button type="button" className="btn" onClick={this.stopPreview}>{'Stop'}</button>
 				<button type="button" className="btn" onClick={this.createSlice}>{'Slice'}</button>
+				<button type="button" className="btn" onClick={this.toggleSubSlice}>{'Toggle Fine Slice'}</button>
 				<p>{`Start: ${this.state.values[0]} Stop: ${this.state.values[1]}`}</p>
 				<div className="slider-container">
 					<ReactSlider 
 						value={this.state.values}
-						max={this.maxValue}
-						min={this.minValue}
-						minDistance={1}
+						max={maxValue}
+						min={minValue}
+						minDistance={this.state.subSlice ? 0.1 : 1}
+						step={this.state.subSlice ? 0.1 : 1}
 						onChange={this.onChange}
 						pearling
 						withBars 
